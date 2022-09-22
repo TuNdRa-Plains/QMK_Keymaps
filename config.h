b63e4ae6915fb6319a2c1c2700d8e59381aac08b
TuNdRa-Plains <114025834+TuNdRa-Plains@users.noreply.github.com>
2022-09-22 22:55:35 +0100

    Add files via upload

--- config.h
+++ config.h
@@ -1,4 +1,5 @@
  /* Copyright 2022 Alex Lee
+  * 2021 HellSingCoder
   *
   * This program is free software: you can redistribute it and/or modify
   * it under the terms of the GNU General Public License as published by
@@ -17,6 +18,9 @@
 
   #pragma once
 
+
+//#define USE_MATRIX_I2C
+
 /* Select hand configuration */
 
 ///https://thomasbaart.nl/2018/12/01/reducing-firmware-size-in-qmk/
@@ -30,6 +34,8 @@
 
 #define CUSTOM_LAYER_READ //if you remove this it causes issues - needs better guarding
 
+// Communication between sides
+#define SOFT_SERIAL_PIN D2 //May Break depending on board layout.
 
 #define TAPPING_FORCE_HOLD
 #ifdef TAPPING_TERM
@@ -38,8 +44,26 @@
 #endif
 #define ENCODER_DIRECTION_FLIP
 
-#define OLED_TIMEOUT 120000
-#define OLED_BRIGHTNESS 120
+// OLED settings
+#ifdef OLED_ENABLE
+	#define OLED_TIMEOUt 6000      		//6000ms - 60 seconds
+	#define OLED_BRIGHTNESS 90			//Default is 100.
+	#define SPLIT_OLED_ENABLE			//Synx on/off OLED state between halves (+100).
+	#ifdef WPM_ENABLE
+		#define SPLIT_WPM_ENABLE			//Enable WPM across split keyboards (+106-268).
+	#endif
+	#define OLED_LOGO					//Enable to print snakey custom logo on slave side (+108).
+	//#define SNEAK_DISABLE				//Disable snakey keyboard pet sneak animation to save space (-132).
+	//#define OLED_NO_SLAVE				//Disable snakey minimal keyboard pet slave OLED rendering (-160).
+	//#define OLED_NO_MASTER				//Disable snakey minimal keyboard pet master OLED rendering and render status on the slave (+96).
+	#ifdef OLED_NO_MASTER
+		#define SPLIT_LAYER_STATE_ENABLE	//Keep on master to save space (+142).
+		#define SPLIT_LED_STATE_ENABLE		//Keep on master to save space (+112).
+		#undef SPLIT_OLED_ENABLE			//Do not sync OLED state with one OLED only (+100).
+	#endif
+	//#define SPLIT_MODS_ENABLE			//Keep on master to save space (+138).
+#endif
+
 #define RGBLIGHT_SLEEP
 //
 #define RGBLIGHT_LAYERS
@@ -48,19 +72,19 @@
 #define RGB_DI_PIN D3
 
 
-#ifdef RGB_MATRIX_ENABLE
+/*#ifdef RGB_MATRIX_ENABLE
 #define RGBLED_NUM 36   // Number of LEDs
 #define RGBLED_NUM 36   // Number of LEDs
 #define DRIVER_LED_TOTAL RGBLED_NUM
-#endif
+#endif */
 
 #ifdef RGBLIGHT_ENABLE
     #undef RGBLED_NUM
 
-    //#define RGBLIGHT_ANIMATIONS
+    #define RGBLIGHT_ANIMATIONS
 	//#define RGBLIGHT_EFFECT_BREATHING
 	//#define RGBLIGHT_EFFECT_RAINBOW_MOOD
-	#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+	//#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
 	//#define RGBLIGHT_EFFECT_SNAKE
 	//#define RGBLIGHT_EFFECT_KNIGHT
 	//#define RGBLIGHT_EFFECT_CHRISTMAS
@@ -80,7 +104,7 @@
     #define RGBLIGHT_VAL_STEP 17
 #endif
 
-#ifdef RGB_MATRIX_ENABLE
+/*#ifdef RGB_MATRIX_ENABLE
 #   define RGB_MATRIX_KEYPRESSES // reacts to keypresses
 // #   define RGB_MATRIX_KEYRELEASES // reacts to keyreleases (instead of keypresses)
 // #   define RGB_DISABLE_AFTER_TIMEOUT 0 // number of ticks to wait until disabling effects
@@ -90,41 +114,41 @@
 // #   define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
 #    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 150 // limits maximum brightness of LEDs to 150 out of 255. Higher may cause the controller to crash.
 
-#define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_GRADIENT_LEFT_RIGHT
+#define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_SOLID_REACTIVE
 
 #    define RGB_MATRIX_HUE_STEP 8
 #    define RGB_MATRIX_SAT_STEP 8
 #    define RGB_MATRIX_VAL_STEP 8
 #    define RGB_MATRIX_SPD_STEP 10
 
-/* Disable the animations you don't want/need.  You will need to disable a good number of these    *
- * because they take up a lot of space.  Disable until you can successfully compile your firmware. */
- // #   undef ENABLE_RGB_MATRIX_ALPHAS_MODS
- // #   undef ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN
- // #   undef ENABLE_RGB_MATRIX_BREATHING
-#   undef ENABLE_RGB_MATRIX_CYCLE_ALL
-#   undef ENABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
-#   undef ENABLE_RGB_MATRIX_CYCLE_UP_DOWN
-#   undef ENABLE_RGB_MATRIX_CYCLE_OUT_IN
-#   undef ENABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
-#   undef ENABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
-#   undef ENABLE_RGB_MATRIX_DUAL_BEACON
-#   undef ENABLE_RGB_MATRIX_RAINBOW_BEACON
-#   undef ENABLE_RGB_MATRIX_RAINBOW_PINWHEELS
-#   undef ENABLE_RGB_MATRIX_RAINDROPS
-#   undef ENABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
- // #   undef ENABLE_RGB_MATRIX_TYPING_HEATMAP
- // #   undef ENABLE_RGB_MATRIX_DIGITAL_RAIN
- // #   undef ENABLE_RGB_MATRIX_SOLID_REACTIVE
- // #   undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
- // #   undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
- // #   undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
- // #   undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
- // #   undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
- // #   undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
- // #   undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
- // #   undef ENABLE_RGB_MATRIX_SPLASH
- // #   undef ENABLE_RGB_MATRIX_MULTISPLASH
- // #   undef ENABLE_RGB_MATRIX_SOLID_SPLASH
- // #   undef ENABLE_RGB_MATRIX_SOLID_MULTISPLASH
-#endif
+ * Disable the animations you don't want/need.  You will need to disable a good number of these    *
+ * because they take up a lot of space.  Disable until you can successfully compile your firmware. 
+  #    undef ENABLE_RGB_MATRIX_ALPHAS_MODS
+  #    undef ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN
+  #    undef ENABLE_RGB_MATRIX_BREATHING
+//#    undef ENABLE_RGB_MATRIX_CYCLE_ALL
+//#    undef ENABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
+//#    undef ENABLE_RGB_MATRIX_CYCLE_UP_DOWN
+//#    undef ENABLE_RGB_MATRIX_CYCLE_OUT_IN
+//#    undef ENABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
+//#    undef ENABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
+//#    undef ENABLE_RGB_MATRIX_DUAL_BEACON
+//#    undef ENABLE_RGB_MATRIX_RAINBOW_BEACON
+//#    undef ENABLE_RGB_MATRIX_RAINBOW_PINWHEELS
+//#    undef ENABLE_RGB_MATRIX_RAINDROPS
+//#    undef ENABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
+  #    undef ENABLE_RGB_MATRIX_TYPING_HEATMAP
+  #    undef ENABLE_RGB_MATRIX_DIGITAL_RAIN
+  #    undef ENABLE_RGB_MATRIX_SOLID_REACTIVE
+  #    undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
+  #    undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
+  #    undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
+  #    undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
+  #    undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
+  #    undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
+  #    undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
+  #    undef ENABLE_RGB_MATRIX_SPLASH
+  #    undef ENABLE_RGB_MATRIX_MULTISPLASH
+  #    undef ENABLE_RGB_MATRIX_SOLID_SPLASH
+  #    undef ENABLE_RGB_MATRIX_SOLID_MULTISPLASH
+#endif */
