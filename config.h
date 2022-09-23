4b89cccfe3c2b07ec0fc452c71a409b49baa955e
TuNdRa-Plains <114025834+TuNdRa-Plains@users.noreply.github.com>
2022-09-23 19:09:14 +0100

    Fixing OLED Behaviour
    Added in provisional RGBLED Layers & support. Mostly lifted straight out of Dave Evans code

--- config.h
+++ config.h
@@ -25,8 +25,8 @@
 
 ///https://thomasbaart.nl/2018/12/01/reducing-firmware-size-in-qmk/
 
-#define SPLIT_USB_DETECT
-//#define MASTER_LEFT
+// #define SPLIT_USB_DETECT
+#define MASTER_LEFT
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
@@ -39,6 +39,7 @@
     #define TAPPING_TERM 200
 #endif
 #define ENCODER_DIRECTION_FLIP
+#define CUSTOM_FONT			
 
 // OLED settings
 #ifdef OLED_ENABLE
@@ -48,10 +49,8 @@
 	#ifdef WPM_ENABLE
 		#define SPLIT_WPM_ENABLE			//Enable WPM across split keyboards (+106-268).
 	#endif
-	#define OLED_LOGO					//Enable to print snakey custom logo on slave side (+108).
-	//#define SNEAK_DISABLE				//Disable snakey keyboard pet sneak animation to save space (-132).
-	//#define OLED_NO_SLAVE				//Disable snakey minimal keyboard pet slave OLED rendering (-160).
-	//#define OLED_NO_MASTER				//Disable snakey minimal keyboard pet master OLED rendering and render status on the slave (+96).
+	#define OLED_LOGO					//Enable to print custom logo on slave side (+108).
+	//#define OLED_NO_MASTER				//Disable keyboard pet master OLED rendering and render status on the slave (+96).
 	#ifdef OLED_NO_MASTER
 		#define SPLIT_LAYER_STATE_ENABLE	//Keep on master to save space (+142).
 		#define SPLIT_LED_STATE_ENABLE		//Keep on master to save space (+112).
@@ -63,29 +62,89 @@
 // Setup RGB Lighting
 /* ws2812 RGB LED */
 #define RGB_DI_PIN D3
-#define DRIVER_LED_TOTAL 36
-#define RGB_MATRIX_SPLIT { 36, 36 } 
-#undef RGB_LEDNUM
-#define RGBLED_NUM 72
-#define RGBLED_SPLIT {36, 36}
 
-#define RGBLIGHT_LIMIT_VAL 120 //Sufficient LEDs on both boards to outrun USB Power over approx 127. 120 should keep it within safe bounds.
 #define RGBLIGHT_SLEEP
-#define RGBLIGHT_SPLIT
+#define RGBLIGHT_LAYERS
 
-/* Define RGB Effects.
- * These are pretty space intensive, so only enable what is wanted. 
- */
-#define RGBLIGHT_MODE_STATIC_LIGHT
- //#define RGBLIGHT_MODE_BREATHING
-#define RGBLIGHT_MODE_RAINBOW_MOOD
- //#define RGBLIGHT_MODE_RAINBOW_SWIRL
- //#define RGBLIGHT_MODE_SNAKE
- //#define RGBLIGHT_MODE_KNIGHT
- //#define RGBLIGHT_MODE_CHRISTMAS
-#define RGBLIGHT_MODE_STATIC_GRADIENT
- //#define RGBLIGHT_MODE_RGB_TEST
- //#define RGBLIGHT_MODE_ALTERNATING
- //#define RGBLIGHT_MODE_TWINKLE
- 
- 
+#ifdef RGB_MATRIX_ENABLE
+    #define RGBLED_NUM 36    // Number of LEDs
+    #define RGBLED_NUM 36    // Number of LEDs
+    #define DRIVER_LED_TOTAL RGBLED_NUM
+	#define RGBLED_SPLIT {36,36
+#endif
+
+#ifdef RGBLIGHT_ENABLE
+    #undef RGBLED_NUM
+
+    //#define RGBLIGHT_ANIMATIONS
+	//#define RGBLIGHT_EFFECT_BREATHING
+	#define RGBLIGHT_EFFECT_RAINBOW_MOOD
+	#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+	//#define RGBLIGHT_EFFECT_SNAKE
+	//#define RGBLIGHT_EFFECT_KNIGHT
+	//#define RGBLIGHT_EFFECT_CHRISTMAS
+	#define RGBLIGHT_EFFECT_STATIC_GRADIENT
+	//#define RGBLIGHT_EFFECT_RGB_TEST
+	//#define RGBLIGHT_EFFECT_ALTERNATING
+	//#define RGBLIGHT_EFFECT_TWINKLE
+
+    #define RGBLED_NUM 72
+	//#define RGBLED_SPLIT
+	#define RGBLED_SPLIT { 36, 36 } // haven't figured out how to use this yet
+
+	//#define RGBLED_NUM 30
+    #define RGBLIGHT_LIMIT_VAL 120
+    #define RGBLIGHT_HUE_STEP 10
+    #define RGBLIGHT_SAT_STEP 17
+    #define RGBLIGHT_VAL_STEP 17
+	#define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_STATIC_GRADIENT
+#endif
+
+#ifdef RGB_MATRIX_ENABLE
+#   define RGB_MATRIX_KEYPRESSES // reacts to keypresses
+// #   define RGB_MATRIX_KEYRELEASES // reacts to keyreleases (instead of keypresses)
+// #   define RGB_DISABLE_AFTER_TIMEOUT 0 // number of ticks to wait until disabling effects
+#   define RGB_DISABLE_WHEN_USB_SUSPENDED // turn off effects when suspended
+#   define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+// #   define RGB_MATRIX_LED_PROCESS_LIMIT (DRIVER_LED_TOTAL + 4) / 5 // limits the number of LEDs to process in an animation per task run (increases keyboard responsiveness)
+// #   define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
+#    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 150 // limits maximum brightness of LEDs to 150 out of 255. Higher may cause the controller to crash.
+
+#define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_GRADIENT_LEFT_RIGHT
+
+#    define RGB_MATRIX_HUE_STEP 8
+#    define RGB_MATRIX_SAT_STEP 8
+#    define RGB_MATRIX_VAL_STEP 8
+#    define RGB_MATRIX_SPD_STEP 10
+
+/* Disable the animations you don't want/need.  You will need to disable a good number of these    *
+ * because they take up a lot of space.  Disable until you can successfully compile your firmware. */
+ // #   undef ENABLE_RGB_MATRIX_ALPHAS_MODS
+ // #   undef ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN
+ // #   undef ENABLE_RGB_MATRIX_BREATHING
+ // #   undef ENABLE_RGB_MATRIX_CYCLE_ALL
+ // #   undef ENABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
+ // #   undef ENABLE_RGB_MATRIX_CYCLE_UP_DOWN
+ // #   undef ENABLE_RGB_MATRIX_CYCLE_OUT_IN
+ // #   undef ENABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
+ // #   undef ENABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
+ // #   undef ENABLE_RGB_MATRIX_DUAL_BEACON
+ // #   undef ENABLE_RGB_MATRIX_RAINBOW_BEACON
+ // #   undef ENABLE_RGB_MATRIX_RAINBOW_PINWHEELS
+ // #   undef ENABLE_RGB_MATRIX_RAINDROPS
+ // #   undef ENABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
+ // #   undef ENABLE_RGB_MATRIX_TYPING_HEATMAP
+ // #   undef ENABLE_RGB_MATRIX_DIGITAL_RAIN
+ // #   undef ENABLE_RGB_MATRIX_SOLID_REACTIVE
+ // #   undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
+ // #   undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
+ // #   undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
+ // #   undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
+ // #   undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
+ // #   undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
+ // #   undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
+ // #   undef ENABLE_RGB_MATRIX_SPLASH
+ // #   undef ENABLE_RGB_MATRIX_MULTISPLASH
+ // #   undef ENABLE_RGB_MATRIX_SOLID_SPLASH
+ // #   undef ENABLE_RGB_MATRIX_SOLID_MULTISPLASH
+#endif
--- keymap.c
+++ keymap.c
@@ -16,7 +16,7 @@
 
 #include QMK_KEYBOARD_H
 
-/*#define INDICATOR_BRIGHTNESS 30
+#define INDICATOR_BRIGHTNESS 30
 
 #define HSV_OVERRIDE_HELP(h, s, v, Override) h, s , Override
 #define HSV_OVERRIDE(hsv, Override) HSV_OVERRIDE_HELP(hsv,Override)
@@ -57,8 +57,7 @@
 		{7, 4, hsv}, \
 	  {35+ 7, 4, hsv}, \
 		{25, 2, hsv}, \
-	  {35+ 25, 2, hsv} */
-
+	  {35+ 25, 2, hsv}
 
 enum sofle_layers {
     /* _M_XYZ = Mac Os, _W_XYZ = Win/Linux */
@@ -460,18 +459,18 @@ static const char PROGMEM mac_logo[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
 static void print_logo_narrow(void) {
     render_logo();
 
-    /* wpm counter */
+    /* wpm counter 
     uint8_t n = get_current_wpm();
     char    wpm_str[4];
-    oled_set_cursor(0, 14);
     wpm_str[3] = '\0';
     wpm_str[2] = '0' + n % 10;
     wpm_str[1] = '0' + (n /= 10) % 10;
-    wpm_str[0] = '0' + n / 10;
-    oled_write(wpm_str, false);
+    wpm_str[0] = '0' + n / 10; */
+	oled_set_cursor(0, 14);
+    oled_write(get_u8_str(get_current_wpm(), '0'), false);
 
     oled_set_cursor(0, 15);
-    oled_write(" wpm", false);
+    oled_write(" WPM", false);
 }
 
 static void print_status_narrow(void) {
@@ -793,4 +792,83 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
     return true;
 }
 
+#endif
+
+
+#ifdef RGBLIGHT_ENABLE
+char layer_state_str[70];
+// Now define the array of layers. Later layers take precedence
+
+// QWERTY,
+// Light on inner column and underglow
+const rgblight_segment_t PROGMEM layer_qwerty_lights[] = RGBLIGHT_LAYER_SEGMENTS(
+  SET_LAYER_ID(HSV_RED)
+
+);
+const rgblight_segment_t PROGMEM layer_colemakdh_lights[] = RGBLIGHT_LAYER_SEGMENTS(
+  SET_LAYER_ID(HSV_PINK)
+);
+
+// _NUM,
+// Light on outer column and underglow
+const rgblight_segment_t PROGMEM layer_num_lights[] = RGBLIGHT_LAYER_SEGMENTS(
+	SET_LAYER_ID(HSV_TEAL)
+
+);
+// _SYMBOL,
+// Light on inner column and underglow
+const rgblight_segment_t PROGMEM layer_symbol_lights[] = RGBLIGHT_LAYER_SEGMENTS(
+	SET_LAYER_ID(HSV_BLUE)
+
+    );
+// _COMMAND,
+// Light on inner column and underglow
+const rgblight_segment_t PROGMEM layer_command_lights[] = RGBLIGHT_LAYER_SEGMENTS(
+  SET_LAYER_ID(HSV_PURPLE)
+);
+
+//_NUMPAD
+const rgblight_segment_t PROGMEM layer_numpad_lights[] = RGBLIGHT_LAYER_SEGMENTS(
+	SET_INDICATORS(HSV_ORANGE),
+    SET_UNDERGLOW(HSV_ORANGE),
+	SET_NUMPAD(HSV_BLUE),
+    {7, 4, HSV_ORANGE},
+    {25, 2, HSV_ORANGE},
+    {35+6, 4, HSV_ORANGE},
+    {35+25, 2, HSV_ORANGE}
+    );
+// _SWITCHER   // light up top row
+const rgblight_segment_t PROGMEM layer_switcher_lights[] = RGBLIGHT_LAYER_SEGMENTS(
+	SET_LAYER_ID(HSV_GREEN),
+	SET_NUMROW(HSV_GREEN)
+);
+
+const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
+
+    layer_qwerty_lights,
+	layer_num_lights,// overrides layer 1
+	layer_symbol_lights,
+    layer_command_lights,
+	layer_numpad_lights,
+	layer_switcher_lights,  // Overrides other layers
+	layer_colemakdh_lights
+);
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+	rgblight_set_layer_state(0, layer_state_cmp(state,_QWERTY));
+
+	rgblight_set_layer_state(1, layer_state_cmp(state, _LOWER));
+	rgblight_set_layer_state(2, layer_state_cmp(state, _RAISE));
+	rgblight_set_layer_state(3, layer_state_cmp(state, _ADJUST));
+	rgblight_set_layer_state(4, layer_state_cmp(state, _NUMPAD));
+	rgblight_set_layer_state(5, layer_state_cmp(state, _SWITCH));
+    return state;
+}
+void keyboard_post_init_user(void) {
+    // Enable the LED layers
+    rgblight_layers = my_rgb_layers;
+
+	rgblight_mode(10);// haven't found a way to set this in a more useful way
+
+}
 #endif
