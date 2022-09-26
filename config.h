d1fb5ddd74738d8386502088b4fcc82149a88f07
TuNdRa-Plains <114025834+TuNdRa-Plains@users.noreply.github.com>
2022-09-26 17:26:14 +0100

    Update config.h

--- config.h
+++ config.h
@@ -1,5 +1,6 @@
  /* Copyright 2022 Alex Lee
   * 2021 HellSingCoder
+  * 2021 Dave Evans
   *
   * This program is free software: you can redistribute it and/or modify
   * it under the terms of the GNU General Public License as published by
